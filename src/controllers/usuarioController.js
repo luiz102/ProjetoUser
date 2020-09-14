0f549ebdf44778c437b35e6148e069f997da8c26
luiz102 <luizranoya@hotmail.com>
2020-09-14 13:58:28 -0300

    Backend com SelectAll e SelectDetail

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -27,3 +27,26 @@ exports.Insert = (req, res, next) => {
         //catch = registra o que queremos que aconteca quando a Promise falhar
         .catch(error => next(error));
 };
+exports.SelectAll = (req,res,next) =>{
+    Usuario.findAll()
+        .then(usuario=>{
+            if (usuario){
+                res.status(status.OK).send(usuario);
+            }
+        })
+        .catch(error =>next(error));
+    }
+ exports.SelectDetail=(req,res,next)=> {
+   const id =req.params.id;
+   
+   Usuario.findByPk(id)
+       .then(usuario=> {
+           if(usuario) {
+               res.status(status.OK).send(usuario);
+           } else {
+               res.status(status.NOT_FOUND).send();
+           }
+       })
+       .catch(error=>next(error));
+ };
+           
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ const UsuarioController = require ('../controllers/usuarioController.js');
 const router = express.Router();
 
 router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios',UsuarioController.SelectAll);
+router.get('/usuarios/:id',UsuarioController.SelectDetail);
  
 module.exports = router;
