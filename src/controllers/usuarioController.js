724453b06676c0c22d4efae617c80017453d0193
luiz102 <luizranoya@hotmail.com>
2020-09-18 15:29:32 -0300

    Backend com Update e Delete

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -49,4 +49,54 @@ exports.SelectAll = (req,res,next) =>{
        })
        .catch(error=>next(error));
  };
-           
+
+ exports.Update = (req,res,next) => {
+     const id = req.params.id;
+     const nome = req.body.nome;
+     const salario = req.body.salario;
+     const dataNascimento = req.body.dataNascimento;
+     const ativo =req.body.ativo;
+
+     Usuario.findByPk(id)
+         .then(usuario  => {
+            if(usuario) {
+                usuario.update({
+                    nome:nome,
+                    salario:salario,
+                    dataNascimento:dataNascimento,
+                    ativo: ativo  
+                },
+                   {
+                       where:{id:id}
+                   })
+                   .then(() => {
+                       res.status(status.OK).send();
+                   })
+                   .catch(error => next(error));
+                } else {
+                    res.status(status.NOT_FOUND).send();
+                }
+            })
+            .catch(error => next(error));
+        };
+        
+    exports.Delete = (req,res,next) => {
+        const id = req.params.id;
+        
+        Usuario.findByPk(id)
+             .then (usuario => {
+                 if (usuario) {
+                     usuario.destroy({
+                         where:{id:id}
+                     })
+                         .then(() => {
+                             res.status(status.OK).send();
+                         })
+                         .catch(error => next(error));
+                        }
+                        else {
+                            res.status(status.NOT_FOUND).send();
+                        }
+             })
+             .catch(error => next(error));
+            };                      
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 router.post('/usuarios', UsuarioController.Insert);
 router.get('/usuarios',UsuarioController.SelectAll);
 router.get('/usuarios/:id',UsuarioController.SelectDetail);
+router.put('/usuarios/:id',UsuarioController.Update);
+router.delete('/usuarios/:id',UsuarioController.Delete);
  
 module.exports = router;
